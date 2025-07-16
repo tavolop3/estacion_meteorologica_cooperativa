d8ab4cc4a687f689fbf46bb3dd126ac78de8f423
tavo <tavo.lop33@gmail.com>
2025-07-16 12:59:58 -0300

    quito coso

--- nodo_cliente/src/config.h
+++ nodo_cliente/src/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 // Datos de la Red WiFi
-#define WIFI_SSID "AFIP 2.4GHz"
-#define WIFI_PASS "v3r0n1c4d5"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 // Datos del Servidor MQTT
 #define MQTT_BROKER "broker.mqtt.cool"
